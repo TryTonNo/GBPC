26e3ebcb90e865f4c582cec023dd1b3e57be6ff2
TryTonNo <pronin.a.a@bk.ru>
2021-01-01 23:42:16 +0300

    Nice print task3:)

--- homeworks/les1/task3.py
+++ homeworks/les1/task3.py
@@ -1,5 +1,11 @@
-n = int(input('Введите число: '))
-# Можно так
-print(n + n * 11 + n * 111)
-# А можно так
-print(n + int(str(n) + str(n)) + int(str(n) + str(n) + str(n)))
+while True:
+    n = input('Введите число: ')
+    if n.isdigit():
+        n = int(n)
+        break
+    else:
+        print('Вы ввели не число. Пожалуйста, введите число.')
+
+rez = f'{n} + {str(n)*2} + {str(n) * 3} = {n + int(str(n) * 2) + int(str(n) * 3)}'
+
+print(rez)
